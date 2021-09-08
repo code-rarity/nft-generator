122820e7ddd48755c9cc8e3c3896f2a3769db1f9
Ajay Alkondon <aalkondon@AAlkondMacPro16.lan>
2021-09-08 16:43:21 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDxy3HT2wf5l_6jYig3jWdWo-PetgSoM8w",
+  authDomain: "nft-generator-8b49d.firebaseapp.com",
+  projectId: "nft-generator-8b49d",
+  storageBucket: "nft-generator-8b49d.appspot.com",
+  messagingSenderId: "910203829001",
+  appId: "1:910203829001:web:0461483c155a165e8871d8",
+  measurementId: "G-WGPM4EPFHB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+import logo from './src/logo.svg';
+import './src/App.css';
 
 function App() {
   return (
