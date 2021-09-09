5b8f481d4d1490b65eed8dc6aac4d97cd2004658
Ajay Alkondon <aalkondon@AAlkondMacPro16.lan>
2021-09-08 17:46:24 -0700

    NFT Generator

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import logo from './logo.svg';
+import './App.css';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,9 +22,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-import logo from './src/logo.svg';
-import './src/App.css';
 
 function App() {
   return (
